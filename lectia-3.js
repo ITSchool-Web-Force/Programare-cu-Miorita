6e8d5581ce93f224ead37a5063f0ca56c4b4d047
Coler Alberto <114001679+coleralberto@users.noreply.github.com>
2023-02-01 18:41:34 +0200

    lectia 1-2-3

--- lectia-3.js
+++ lectia-3.js
@@ -1,3 +1,11 @@
 // https://miorita.scoalaweb.ro/lessons/3-while
 // Scrie un program care să o ajute pe Miorița să ajungă la pășunea din centrul spiralei
 // Poți folosi oricare din conceptele învățate la curs, chiar și de la lecția a 4-a.
+reset();
+
+while (canMove()) {
+  move();
+  if (!canMove()) {
+    turnRight();
+  }
+}
