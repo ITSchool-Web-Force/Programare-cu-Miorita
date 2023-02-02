8a420f78760cac5dfd048c1865b9b7ecde699aa1
Claudiu <nistor.claudiu92@gmail.com>
2023-02-02 18:10:29 +0200

    Varianta fara else

--- lectia-3.js
+++ lectia-3.js
@@ -5,3 +5,24 @@ while(canMove()) {
                 turnRight()
         }
 }
+
+
+Fara else mai jos:
+
+while(canMove()) {
+        move()
+        if(!canMove() ) {
+            turnRight()
+        }
+    }
+
+    var iteration = 1
+
+    while (iteration < 7) {
+        move()
+        iteration = iteration + 1;
+    }
+
+    for (var i = 1; i < 7; i = i + 1) {
+        move()
+    }
