0625606c6c3e84c96002ac7d6e9be8e5a9b0f91a
Claudiu <nistor.claudiu92@gmail.com>
2023-02-02 13:43:37 +0200

    Miorita

--- playground-1.js
+++ playground-1.js
@@ -1,2 +1,7 @@
-// https://miorita.scoalaweb.ro/playgrounds/index
-// Fă-o pe Miorița să ajungă în fiecare din cele 4 colțuri ale stânei
+reset()
+while(canMove()) {
+        move()
+        if(!canMove()) {
+        turnRight()
+}
+}
