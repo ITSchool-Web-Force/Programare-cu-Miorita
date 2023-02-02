4f3121c4d9f4944599ca47296a05716afe18d197
Octavian Busuioc <octavianbusuioc184@yahoo.ro>
2023-02-02 13:30:56 +0200

    lectiile 1, 2 ,3

--- lectia-2.js
+++ lectia-2.js
@@ -1,3 +1,27 @@
 // https://miorita.scoalaweb.ro/lessons/2-if
 // Scrie un program care să o ajute pe Miorița să ajungă la oricare din pășuni
 // Programul trebuie să funcționeze și cu gard, și fără gard
+
+reset();
+
+if (!canMove()) {
+  //cu gard
+  turnRight();
+  turnRight();
+  turnRight();
+  move();
+  turnRight();
+  move();
+  turnRight();
+  move();
+  turnRight();
+  turnRight();
+  turnRight();
+  move();
+  move();
+} else {
+  //in cazul in care nu e gard
+  move();
+  move();
+  move();
+}
--- lectia-3.js
+++ lectia-3.js
@@ -1,3 +1,20 @@
 // https://miorita.scoalaweb.ro/lessons/3-while
 // Scrie un program care să o ajute pe Miorița să ajungă la pășunea din centrul spiralei
 // Poți folosi oricare din conceptele învățate la curs, chiar și de la lecția a 4-a.
+
+reset();
+
+let furryThing;
+furryThing = canMove();
+
+while (furryThing) {
+  let fuzzyThing;
+  fuzzyThing = canMove();
+
+  if (!fuzzyThing) {
+    turnRight();
+    continue;
+  }
+  //momentan parcurge traseul pana la final si apoi ruleaza la infinit
+  move();
+}
