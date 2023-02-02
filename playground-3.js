fc318f57f770fdd0896cd9c3af62fcdc6fdf5016
elenatabacaru <120025210+elenatabacaru@users.noreply.github.com>
2023-02-02 15:16:57 +0200

    Miorita

--- playground-3.js
+++ playground-3.js
@@ -1,3 +1,32 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
 // Programul trebuie să funcționeze indiferent de configurația stânei.
+
+reset();
+var free = canMove();
+while (!free) {
+    turnRight();
+    free = canMove();
+    if (free) {
+        move();
+        break;
+    }
+    else {
+        turnRight();
+    }
+    free = canMove();
+}
+while (free) {
+    free = canMove();
+    if (!free) {
+        break;
+    }
+    else {
+        move();
+        free = canMove();
+            if (!free) 
+                break;
+            else 
+                move();
+    }
+ }
