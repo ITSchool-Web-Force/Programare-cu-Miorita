213c431bab10449b2ee0f3f634cc17d879eb101d
Claudiu <nistor.claudiu92@gmail.com>
2023-02-02 18:28:40 +0200

    Lectia 2 fara else

--- lectia-2.js
+++ lectia-2.js
@@ -12,3 +12,20 @@ else {
         move()
         move()
 }
+
+fara else
+
+reset()
+if ( canMove() ) {
+        move()
+        move()
+        move()
+}
+if ( !canMove() ) {
+        turnLeft()
+        move()
+        turnRight()
+        move()
+        move()
+        move()
+}
