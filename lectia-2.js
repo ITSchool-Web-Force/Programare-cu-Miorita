ea72bae33aeeab8c866207fb09d53c4569f11a48
Alessia Rădos <rados.alessia.elena@gmail.com>
2023-02-02 18:02:16 +0200

    rezolvare primele 2 lectii

--- lectia-2.js
+++ lectia-2.js
@@ -1,3 +1,17 @@
 // https://miorita.scoalaweb.ro/lessons/2-if
 // Scrie un program care să o ajute pe Miorița să ajungă la oricare din pășuni
 // Programul trebuie să funcționeze și cu gard, și fără gard
+reset();
+if(!canMove())
+{turnLeft();
+move();
+turnRight();
+move();
+move();
+move();
+}
+else
+{move();
+move();
+move();
+}
