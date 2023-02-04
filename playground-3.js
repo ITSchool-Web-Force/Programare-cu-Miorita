01aec68c478bd208cee79df411eb0ace240181f3
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-02-04 13:29:40 +0200

    playground3

--- playground-3.js
+++ playground-3.js
@@ -1,3 +1,14 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
 // Programul trebuie să funcționeze indiferent de configurația stânei.
+
+let i = 0;
+for(i=1;i<3;i++){
+    while(canMove()){
+        move();
+        i=i+1;
+    }
+    while(!canMove()){
+        turnRight();
+    }
+}
