57ed6a141c77049f9fc32953c1f35eee66f996fc
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-02-04 13:20:04 +0200

    playground2

--- playground-2.js
+++ playground-2.js
@@ -1,2 +1,27 @@
 // https://miorita.scoalaweb.ro/playgrounds/hatchet
 // Scrie un program care să o ajute pe Miorița să ridice baltagul și să îl aducă Vitoriei
+
+function pickHatchet() {
+    pick("hatchet");
+}
+function dropHatchet() {
+    drop("hatchet");
+}
+
+
+let i = 0;
+    for(i=0;i<1;i++){
+        move();
+        pickHatchet();
+        i=i+1;
+        }
+    for(i=1;i<6;i++){
+        while(canMove()){
+            move();
+            i=i+1;
+        }
+        while(!canMove()){
+            turnRight();
+        }
+        }
+dropHatchet()
