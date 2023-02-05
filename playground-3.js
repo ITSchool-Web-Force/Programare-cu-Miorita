55dd3af3928cc57c255c3e25f8761dc83462d693
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-02-05 19:15:31 +0200

    playground-3

--- playground-3.js
+++ playground-3.js
@@ -1,3 +1,16 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
 // Programul trebuie să funcționeze indiferent de configurația stânei.
+
+reset();
+
+while (true) {
+  turnRight();
+  if(canMove()) {
+    break;
+  }
+}
+
+while (canMove()) {
+    move();
+}
