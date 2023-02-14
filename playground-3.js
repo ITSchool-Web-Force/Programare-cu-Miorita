640f4e07dd90abb6f8486621e56331a9882103ec
Cervencu George <120504020+GGC911@users.noreply.github.com>
2023-02-14 18:11:40 +0200

    2-3

--- playground-3.js
+++ playground-3.js
@@ -1,3 +1,30 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
 // Programul trebuie să funcționeze indiferent de configurația stânei.
+reset();
+
+let free = true;
+
+while (free) {
+  if (canMove()) {
+    move();
+  } else {
+    while (!canMove()) {
+      turnRight();
+    }
+  }
+  if (!canMove()) {
+    break;
+  }
+}
+
+// sau:
+
+reset();
+
+while (!canMove()) {
+  turnRight();
+}
+while (canMove()) {
+  move();
+}
