95dcbda71ddabc31c387efbb6c52b57890ba4f0d
Cervencu George <120504020+GGC911@users.noreply.github.com>
2023-02-15 12:36:30 +0200

    p1

--- playground-1.js
+++ playground-1.js
@@ -1,2 +1,17 @@
 // https://miorita.scoalaweb.ro/playgrounds/index
 // Fă-o pe Miorița să ajungă în fiecare din cele 4 colțuri ale stânei
+reset();
+for (let i = 0; i < 28; i++) {
+  canMove() ? move() : turnRight();
+}
+
+reset();
+
+for (let i = 0; i < 5; i++) {
+  while (canMove()) {
+    move();
+  }
+  if (!canMove()) {
+    turnRight();
+  }
+}
