1be3fc0bf04815d4334adaa090b6785ce5747ae1
Alessia Rădos <rados.alessia.elena@gmail.com>
2023-02-28 18:26:01 +0200

    Probleme playground

--- playground-1.js
+++ playground-1.js
@@ -1,2 +1,12 @@
 // https://miorita.scoalaweb.ro/playgrounds/index
 // Fă-o pe Miorița să ajungă în fiecare din cele 4 colțuri ale stânei
+
+for(i=0; i<5;i++)
+{
+    while(canMove())
+    {   
+        move();
+    
+    }
+    turnRight();
+}
--- playground-2.js
+++ playground-2.js
@@ -1,2 +1,16 @@
 // https://miorita.scoalaweb.ro/playgrounds/hatchet
 // Scrie un program care să o ajute pe Miorița să ridice baltagul și să îl aducă Vitoriei
+move();
+pick("hatchet");
+turnRight();
+while(canMove())
+{
+    move();
+}
+turnLeft();
+
+while(canMove())
+{
+    move();
+}
+drop("hatchet");
