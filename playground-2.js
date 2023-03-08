563b7b7df728741b0f7281b690004d5a533263b4
Crușoveanu Ioan <ionut.cruso@gmail.com>
2023-03-09 01:59:28 +0200

    playgrounds

--- playground-2.js
+++ playground-2.js
@@ -1,2 +1,16 @@
 // https://miorita.scoalaweb.ro/playgrounds/hatchet
 // Scrie un program care să o ajute pe Miorița să ridice baltagul și să îl aducă Vitoriei
+
+reset();
+let i=1;
+move();
+pick("hatchet");
+while(canMove()){
+    move();
+    if(!canMove() && i<2){
+        turnRight();
+        i++;
+        continue;
+    }  
+}
+drop("hatchet");
--- playground-3.js
+++ playground-3.js
@@ -1,3 +1,17 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
 // Programul trebuie să funcționeze indiferent de configurația stânei.
+
+reset();
+let i=1;
+turnRight();
+turnRight();
+turnRight();
+while(canMove()){
+    move();
+    if(!canMove() && i<5){
+        turnRight();
+        i++;
+        continue;
+    }  
+}
